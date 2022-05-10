12edcf524c6381546a20f710ad3cf728f05fb044
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-05-10 15:48:29 -0300

    initial commit

---
+++ pages/api/contexts/steam/profile/[id].ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import nc from "next-connect";
+
+const router = nc<NextApiRequest,NextApiResponse>({
+  onError: (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).end("¡Algo se rompió!");
+  },
+  onNoMatch: (req, res) => {
+    res.status(404).end("¡No lo encontré che!");
+  },
+})
+
+type Data = {
+  name: string
+}
+
+router.get(async (req, res) => {
+  const userId = req.query.id as string
+  const key = process.env.STEAM_TOKEN
+  const queries = `?key=${key}&steamids=${userId}&format=json`
+  const url = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/${queries}`
+  const response = await fetch(url)
+  const data = await response.json()
+  
+  res.status(200).json(data)
+})
+
+export default router
